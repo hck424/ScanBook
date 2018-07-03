a5821501ccff7d3c2cc4a14c186402c51db9fe80
학철 <hagcheol@iphone-devpsynetcokrui-macbook-pro.local>
2018-07-03 17:21:04 +0900

    Initial Commit

---
+++ ScanBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanBook
+//
+//  Created by 학철 on 2018. 7. 3..
+//  Copyright © 2018년 학철. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanBook
+//
+//  Created by 학철 on 2018. 7. 3..
+//  Copyright © 2018년 학철. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
